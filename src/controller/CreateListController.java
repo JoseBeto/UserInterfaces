ef38db38928977fdc310b4de897ab95bfa107c22
Jose Bocanegra <jbocanegra96@gmail.com>
2018-03-29 11:55:17 -0500

    Adding create list view and controller

---
+++ src/controller/CreateListController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateListController {
+	
+	@FXML private TextField listNameText;
+
+	public CreateListController() {
+		
+	}
+	
+    @FXML
+    void createListButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButtonClicked(ActionEvent event) {
+
+    }
+}
