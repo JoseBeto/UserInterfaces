081503510220cad26164dfdb75b39b7c161fa340
Jose Bocanegra <JBocanegra96@gmail.com>
2018-04-02 08:26:31 -0500

    Changed add payment method view

--- src/controller/AddPaymentMethodController.java
+++ src/controller/AddPaymentMethodController.java
@@ -21,6 +21,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -34,8 +35,6 @@ public class AddPaymentMethodController implements Initializable, MyController
 	@FXML BorderPane paymentPane;
 	@FXML GridPane paypalGrid;
 	@FXML GridPane cardGrid;
-	@FXML GridPane bankGrid;
-	@FXML GridPane coinGrid;
 	
 	@FXML TextField paypalEmailText;
 	@FXML PasswordField paypalPassText;
@@ -58,17 +57,80 @@ public class AddPaymentMethodController implements Initializable, MyController
 		this.view = view;
 	}
 	
+	@FXML
+    void backButtonClicked(MouseEvent event) {
+		if(view == AppController.CHECK_OUT)
+			AppController.getInstance().changeView(view, null);
+		else if(view == AccountController.PAYMETHODS)
+			AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
+		else if(view == AccountController.INFO)
+			AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
+    }
+
+	//Action Event Listener for Category Box
+	@FXML
+	public void paymentMethodSelected(ActionEvent event) throws IOException
+	{
+		switch(paymentMethodBox.getSelectionModel().getSelectedIndex())
+		{
+			case 0: //Buy
+				paymentPane.setCenter(paypalGrid);
+				break;
+			case 1: //Sell
+				paymentPane.setCenter(cardGrid);
+				break;
+		}
+	}
+	
+	@FXML
+	public void submitPressed(ActionEvent event) throws IOException
+	{
+		PaymentMethod payMethod = null;	
+		if( paymentPane.getCenter() == null )
+		{
+			System.out.println("You have not selected a payment method.");
+		}
+		else
+		{
+			switch(paymentMethodBox.getSelectionModel().getSelectedIndex())
+			{
+			case 0: //Buy
+				payMethod = new PaypalMethod();
+				payMethod.fillMethodDetails(paypalEmailText.getText(), paypalPassText.getText());
+				break;
+			case 1: //Sell
+				payMethod = new CardPayment();
+				payMethod.fillMethodDetails(cardNumberText.getText(), expDateText.getText(), fullnameText.getText());
+				break;
+			}
+			if(!payMethod.validateMethod()) //Validation failed
+				return;
+
+			AlertHelper.showWarningMessage("Success!", "Payment method added!", AlertType.INFORMATION);
+
+			payGateway.addPaymentMethod(payMethod);
+			user.addPaymentMethod(payMethod);
+			userGateway.updatePaymentMethods(user);
+
+			if(view == AppController.CHECK_OUT)
+				AppController.getInstance().changeView(view, null);
+			else if(view == AccountController.PAYMETHODS)
+				AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
+			else if(view == AccountController.INFO)
+				AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
+
+		}
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1)
 	{
 		// TODO Auto-generated method stub
-		methodsList = FXCollections.observableArrayList("Paypal", "Credit Card", "Bank Account", "Bitcoin");
+		methodsList = FXCollections.observableArrayList("Paypal", "Credit Card");
 		paymentMethodBox.getItems().setAll(methodsList);
 
 		paymentPane.setCenter(null);
 		paymentPane.setLeft(null);
-		paymentPane.setRight(null);
-		paymentPane.setBottom(null);
 
 		//Prevents user from entering a non digit
 		cardNumberText.textProperty().addListener(new ChangeListener<String>() {
@@ -101,80 +163,5 @@ public class AddPaymentMethodController implements Initializable, MyController
 				}
 			}
 		});
-	}
-
-	//Action Event Listener for Category Box
-	@FXML
-	public void paymentMethodSelected(ActionEvent event) throws IOException
-	{
-		switch(paymentMethodBox.getSelectionModel().getSelectedIndex())
-		{
-			case 0: //Buy
-				paymentPane.setCenter(paypalGrid);
-				break;
-			case 1: //Sell
-				paymentPane.setCenter(cardGrid);
-				break;
-			case 2: //Contact Us
-				System.out.println("You selected Bank Account.");
-				//paymentPane.setCenter(null);
-				paymentPane.setCenter(bankGrid);
-				break;
-			case 3: //About
-				System.out.println("You selected Bitcoin");
-				//paymentPane.setCenter(null);
-				paymentPane.setCenter(coinGrid);
-				break;
-		}
-	}
-	
-	@FXML
-	public void submitPressed(ActionEvent event) throws IOException
-	{
-		PaymentMethod payMethod = null;	
-		if( paymentPane.getCenter() == null )
-		{
-			System.out.println("You have not selected a payment method.");
-		}
-		else
-		{
-			switch(paymentMethodBox.getSelectionModel().getSelectedIndex())
-			{
-			case 0: //Buy
-				payMethod = new PaypalMethod();
-				payMethod.fillMethodDetails(paypalEmailText.getText(), paypalPassText.getText());
-				break;
-			case 1: //Sell
-				payMethod = new CardPayment();
-				payMethod.fillMethodDetails(cardNumberText.getText(), expDateText.getText(), fullnameText.getText());
-				break;
-			case 2: //Contact Us
-				System.out.println("You selected Bank Account.");
-				//paymentPane.setCenter(null);
-				paymentPane.setCenter(bankGrid);
-				break;
-			case 3: //About
-				System.out.println("You selected Bitcoin");
-				//paymentPane.setCenter(null);
-				paymentPane.setCenter(coinGrid);
-				break;
-			}
-			if(!payMethod.validateMethod()) //Validation failed
-				return;
-
-			AlertHelper.showWarningMessage("Success!", "Payment method added!", AlertType.INFORMATION);
-
-			payGateway.addPaymentMethod(payMethod);
-			user.addPaymentMethod(payMethod);
-			userGateway.updatePaymentMethods(user);
-
-			if(view == AppController.CHECK_OUT)
-				AppController.getInstance().changeView(view, null);
-			else if(view == AccountController.PAYMETHODS)
-				AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
-			else if(view == AccountController.INFO)
-				AppController.getInstance().changeView(AppController.MY_ACCOUNT, view);
-
-		}
 	}
 }
